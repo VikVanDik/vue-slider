99f852c2f161f94a0ca3ec0123b124a0a7873f23
VikVanDik <95vlc17@gmail.com>
2023-09-28 16:40:47 +0200

    [add]: methods

--- script/script.js
+++ script/script.js
@@ -10,7 +10,12 @@ createApp ({
       texts : ['Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.', 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.', "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.", 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city', "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay."]
     }
   },
-  
+
+  methods : {
+    isActive : true,
+    counter : 0
+  },
+
   mounted () {
     console.log(this.titles);
   }
